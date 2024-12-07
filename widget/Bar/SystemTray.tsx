5d5141329435512fd528795bdc9f2d3c3ab34fdb
anders130 <93037023+anders130@users.noreply.github.com>
2024-12-07 01:11:52 +0100

    added primary button function to system tray

--- widget/Bar/SystemTray.tsx
+++ widget/Bar/SystemTray.tsx
@@ -1,4 +1,4 @@
-import { App, Gdk } from "astal/gtk3"
+import { Astal, Gdk } from "astal/gtk3"
 import { bind } from "astal"
 import Tray from "gi://AstalTray"
 
@@ -9,21 +9,21 @@ export default function SystemTray() {
         <box>
             {bind(tray, "items").as((items) =>
                 items.map((item) => {
-                    if (item.iconThemePath) App.add_icons(item.iconThemePath)
-
                     const menu = item.create_menu()
-
                     return (
                         <button
                             tooltipMarkup={bind(item, "tooltipMarkup")}
                             onDestroy={() => menu?.destroy()}
-                            onClickRelease={(self) => {
-                                menu?.popup_at_widget(
-                                    self,
-                                    Gdk.Gravity.SOUTH,
-                                    Gdk.Gravity.NORTH,
-                                    null,
-                                )
+                            onClickRelease={(self, e) => {
+                                if (e.button === Astal.MouseButton.PRIMARY)
+                                    item.activate(e.x, e.y)
+                                if (e.button === Astal.MouseButton.SECONDARY)
+                                    menu?.popup_at_widget(
+                                        self,
+                                        Gdk.Gravity.SOUTH,
+                                        Gdk.Gravity.NORTH,
+                                        null,
+                                    )
                             }}
                         >
                             <icon gIcon={bind(item, "gicon")} />
