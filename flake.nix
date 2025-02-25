128f3fc463bfdbc63601bd9a8f9945eba69134fe
anders130 <93037023+anders130@users.noreply.github.com>
2025-02-25 15:23:39 +0100

    add missing dependencies

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
     outputs = inputs: let
         system = "x86_64-linux";
         pkgs = inputs.nixpkgs.legacyPackages.${system};
+        deps = [pkgs.pulseaudio];
     in {
         packages.${system}.default = inputs.ags.lib.bundle {
             inherit pkgs;
@@ -20,16 +21,17 @@
             name = "my-shell";
             entry = "app.ts";
 
-            extraPackages = with inputs.ags.packages.${system}; [
-                hyprland
-                tray
-            ];
+            extraPackages =
+                deps
+                ++ (with inputs.ags.packages.${system}; [
+                    hyprland
+                    tray
+                    apps
+                ]);
         };
 
         devShells.${system}.default = pkgs.mkShell {
-            buildInputs = [
-                inputs.ags.packages.${system}.agsFull
-            ];
+            buildInputs = deps ++ [inputs.ags.packages.${system}.agsFull];
         };
     };
 }
